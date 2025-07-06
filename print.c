6fd2f97d9c664c18e418cf565627f5be38c133b3
erashu122 <kbitu8676@gmail.com>
2025-07-06 15:28:58 +0530

    Initial commit

---
+++ print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    printf("hello world\n");
+    printf("my age is %d\n", 25);
+    printf("my height is %.2f\n", 5.9); 
+    return 0;           
+}   
+//this is single line comment
+/* this is
+multi line comment
+   this is also multi line comment */   
+// This is a comment at the end of the file
+// The code above prints "hello world", my age, and my height to the console.
